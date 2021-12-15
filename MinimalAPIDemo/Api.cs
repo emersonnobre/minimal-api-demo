ed5523cdc4b0ae24da5c16e84f1a49d8149f0adf
emersonnobre <egrnobre@gmail.com>
2021-12-14 21:39:29 -0400

    Adding the API file wich contains all endpoints of the app.

---
+++ MinimalAPIDemo/Api.cs
@@ -0,0 +1,21 @@
+﻿namespace MinimalAPIDemo
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapGet("/Users", GetUsers);
+        }
+
+        private static async Task<IResult> GetUsers(IUserData data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetUsers());
+            } catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+    }
+}
